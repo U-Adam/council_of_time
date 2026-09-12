332220fdc56bd308f467abf9b59f0427c85260e5
U-Adam <269580877+U-Adam@users.noreply.github.com>
2026-09-12 13:28:05 -0700

    Route /about to the Council methodology page

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
+import { AboutPage } from "./AboutPage";
 import "./styles.css";
 import "./attribution.css";
 import "./artist-witness.css";
 import "./about.css";
 
+const isAboutPage = window.location.pathname === "/about" || window.location.pathname.startsWith("/about/");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    {isAboutPage ? <AboutPage /> : <App />}
   </React.StrictMode>,
 );
