cf1d3f7d35fce719229d7e2b1dc052a8ad03f831
U-Adam <269580877+U-Adam@users.noreply.github.com>
2026-09-16 16:59:28 -0700

    Stop runtime favicon from reverting to legacy asset

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ const isAboutPage = window.location.pathname === "/about" || window.location.pat
 const favicon = document.querySelector<HTMLLinkElement>('link[rel="icon"]') ?? document.createElement("link");
 favicon.rel = "icon";
 favicon.type = "image/svg+xml";
-favicon.href = "/council-mark-small.svg";
+favicon.href = "/favicon-v2.svg";
 if (!favicon.parentNode) document.head.appendChild(favicon);
 
 function HomeApp() {
